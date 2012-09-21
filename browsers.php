ba8221c4b924456b2af7655c25b5fc51a16855f3
Steve Barnett <steve@naga.co.za>
2012-09-21 09:44:33 +0200

    Tweak browser list

--- browsers.php
+++ browsers.php
@@ -1,6 +1,6 @@
 <?php
-$title = 'Schedule | Nomad Device Lab';
-$active = 'schedule';
+$title = 'Browser Locations | Nomad Device Lab';
+$active = 'browser-locations';
 require('_header.php');
 ?>
 
@@ -11,42 +11,57 @@ require('_header.php');
 
 <ul>
   <li>Nokia XpressMusic 5310
-
   <ul>
-  <li>Native. Menu &gt; Web &gt; Go to address</li>
-  <li>Nokia Browser beta. Menu &gt; Applications &gt; Games</li>
-  <li>Opera Mini. Menu &gt; Applications &gt; Collection</li>
+    <li>Native. Menu &gt; Web &gt; Go to address</li>
+    <li>Nokia Browser beta. Menu &gt; Applications &gt; Games</li>
+    <li>Opera Mini. Menu &gt; Applications &gt; Collection</li>
   </ul></li>
+
   <li>Nokia N97
-
   <ul>
-  <li>Native: Menu button &gt; Web</li>
-  <li>Opera Mini, Opera Mobile: Menu button &gt; applications &gt;</li>
-  <li>UC browser. Menu button &gt; applications</li>
+    <li>Native: Menu button &gt; Web</li>
+    <li>Opera Mini, Opera Mobile: Menu button &gt; applications</li>
+    <li>UC browser. Menu button &gt; applications</li>
   </ul></li>
+  
   <li>Blackberry 8520
-
   <ul>
-  <li>Native: home page, tray - Browser</li>
-  <li>Opera Mini: BB button &gt; Downloads</li>
+    <li>Native: home page, tray - Browser</li>
+    <li>Opera Mini: BB button &gt; Downloads</li>
   </ul></li>
+  
   <li>iPad
-
   <ul>
-  <li>safari: tray</li>
-  <li>other: last page</li>
+    <li>safari: tray</li>
+    <li>other: last page</li>
   </ul></li>
+
   <li>iPhone 4
-
   <ul>
-  <li>Safari: front page</li>
-  <li>others: last page</li>
+    <li>Safari: front page</li>
+    <li>others: last page</li>
   </ul></li>
+
   <li>Kindle
-
   <ul>
-  <li>Home button, menu button &gt; Experimental, web browser</li>
+    <li>Home button, menu button &gt; Experimental, web browser</li>
   </ul></li>
+
+  <li>Samsung Galaxy Pocket
+  <ul>
+    <li>Page 1</li>
+  </ul></li>
+
+  <li>Alacatel One Touch
+  <ul>
+    <li>Page 1</li>
+  </ul></li>
+</ul>
+
+<hr />
+
+<ul>
+
   <li>Laptop
 
   <ul>
@@ -56,16 +71,7 @@ require('_header.php');
   <li>Launcher</li>
   </ul></li>
   </ul></li>
-  <li>Samsung Galaxy Pocket
 
-  <ul>
-  <li>Page 1</li>
-  </ul></li>
-  <li>Alacatel One Touch
-
-  <ul>
-  <li>Page 1</li>
-  </ul></li>
 </ul>
 
 
