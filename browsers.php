4d731ec045cf93986549e83d766f8d5d245257b5
Steve Barnett <steve@naga.co.za>
2012-09-21 09:38:19 +0200

    Add browser locations

---
+++ browsers.php
@@ -0,0 +1,75 @@
+<?php
+$title = 'Schedule | Nomad Device Lab';
+$active = 'schedule';
+require('_header.php');
+?>
+
+<section class="sessions-next">
+  <header>
+    <h2>Browser locations on devices</h2>
+  </header>
+
+<ul>
+  <li>Nokia XpressMusic 5310
+
+  <ul>
+  <li>Native. Menu &gt; Web &gt; Go to address</li>
+  <li>Nokia Browser beta. Menu &gt; Applications &gt; Games</li>
+  <li>Opera Mini. Menu &gt; Applications &gt; Collection</li>
+  </ul></li>
+  <li>Nokia N97
+
+  <ul>
+  <li>Native: Menu button &gt; Web</li>
+  <li>Opera Mini, Opera Mobile: Menu button &gt; applications &gt;</li>
+  <li>UC browser. Menu button &gt; applications</li>
+  </ul></li>
+  <li>Blackberry 8520
+
+  <ul>
+  <li>Native: home page, tray - Browser</li>
+  <li>Opera Mini: BB button &gt; Downloads</li>
+  </ul></li>
+  <li>iPad
+
+  <ul>
+  <li>safari: tray</li>
+  <li>other: last page</li>
+  </ul></li>
+  <li>iPhone 4
+
+  <ul>
+  <li>Safari: front page</li>
+  <li>others: last page</li>
+  </ul></li>
+  <li>Kindle
+
+  <ul>
+  <li>Home button, menu button &gt; Experimental, web browser</li>
+  </ul></li>
+  <li>Laptop
+
+  <ul>
+  <li>Ubuntu
+
+  <ul>
+  <li>Launcher</li>
+  </ul></li>
+  </ul></li>
+  <li>Samsung Galaxy Pocket
+
+  <ul>
+  <li>Page 1</li>
+  </ul></li>
+  <li>Alacatel One Touch
+
+  <ul>
+  <li>Page 1</li>
+  </ul></li>
+</ul>
+
+
+</section>
+
+<?php
+require('_footer.php');
