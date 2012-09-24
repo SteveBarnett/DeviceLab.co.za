4375e032ef61d7ec94929900e0eda52370813f85
Steve Barnett <steve@naga.co.za>
2012-09-24 13:28:29 +0200

    Access key support, CodeKit tweaks

--- _footer.php
+++ _footer.php
@@ -27,15 +27,15 @@
       <h1>Main Navigation</h1>
     </header>
     <ul>
-      <li<?php if($active == 'index') echo ' class="nav-active"'; ?>><a href="index.php">home</a></li>
-      <li<?php if($active == 'mobile-statistics') echo ' class="nav-active"'; ?>><a href="mobile-statistics.php">mobile statistics</a></li>
-      <li<?php if($active == 'testing-on-mobile') echo ' class="nav-active"'; ?>><a href="testing-on-mobile.php">testing on mobile</a></li>
-      <li<?php if($active == 'the-lab') echo ' class="nav-active"'; ?>><a href="the-lab.php">the lab</a></li>
-      <li<?php if($active == 'schedule') echo ' class="nav-active"'; ?>><a href="schedule.php">schedule</a></li>
+      <li<?php if($active == 'index') echo ' class="nav-active"'; ?>><a href="index.php" accesskey="h">home</a></li>
+      <li<?php if($active == 'mobile-statistics') echo ' class="nav-active"'; ?>><a href="mobile-statistics.php" accesskey="m">mobile statistics</a></li>
+      <li<?php if($active == 'testing-on-mobile') echo ' class="nav-active"'; ?>><a href="testing-on-mobile.php" accesskey="t">testing on mobile</a></li>
+      <li<?php if($active == 'the-lab') echo ' class="nav-active"'; ?>><a href="the-lab.php" accesskey="l">the lab</a></li>
+      <li<?php if($active == 'schedule') echo ' class="nav-active"'; ?>><a href="schedule.php" accesskey="s">schedule</a></li>
     </ul>
   </nav>
 
-<p id="back" class="navigation"><a href="#top">Back to top of page &uarr;</a></p>  
+<p id="back" class="navigation"><a href="#top" accesskey="b">Back to top of page &uarr;</a></p>  
 
 <?php /*
   <!-- JavaScript at the bottom for fast page loading -->
