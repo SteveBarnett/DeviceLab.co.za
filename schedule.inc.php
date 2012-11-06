ee8ed5498fc455708b328d7f0f814b226ea59ef7
Steve Barnett <steve@naga.co.za>
2012-11-06 11:12:13 +0200

    Added new dates to schedule

--- schedule.inc.php
+++ schedule.inc.php
@@ -24,6 +24,14 @@ $schedule = array(
     'status' => 'Full booked, sorry!'
   ),
 
+    '2012-11-08T13:00' => array(
+    'dtend' => '2012-11-08T16:00',
+    'location' => 'Information Logistics',
+    'url' => 'http://www.informationlogistics.co.za/',
+    'contact' => 'Steve Barnett',
+    'contact-email' => 'nomad@devicelab.co.za'
+  ),
+
   // '2011-10-09T14:00' => array(
   //   'dtend' => '2012-10-09T17:00',
   //   'location' => 'The Bandwidth Barn',
