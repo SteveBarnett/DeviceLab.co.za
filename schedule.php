d3b073b718b1e27628fff1787a0339b9331ddb43
Steve Barnett <steve@naga.co.za>
2013-02-01 14:16:23 +0200

    Updated Schedule page.

--- schedule.php
+++ schedule.php
@@ -9,9 +9,11 @@ require('_header.php');
     <h2>Sessions</h2>
   </header>
 
-  <p>The listing of sessions has moved to <a href="http://www.meetup.com/Nomad-Device-Lab/">Meetup.com/Nomad-Device-Lab</a>.</p>
-  
-  <p>Please check there for the most up-to-date information.</p>
+  <p>The lab now has a permanent home at my workplace: <a href="http://www.unboxedconsulting.com/">Unboxed Consulting</a> in Constantia. <a href="http://goo.gl/maps/SxLkY">View Google Map of the location</a>.<br />
+  If you'd like to come and do some testing, please <strong>make an appointment</strong> by dropping me a line to <a class="email" href="mailto:nomad@devicelab.co.za">nomad@devicelab.co.za</a>.</p>
+
+  <p>The listing of sessions from 2012 is up at <a href="http://www.meetup.com/Nomad-Device-Lab/">Meetup.com/Nomad-Device-Lab</a>.</p>
+
 
 </section>
 
