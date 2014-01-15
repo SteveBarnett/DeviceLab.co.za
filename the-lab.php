a46624465cbc86161ab737618808299574adf589
Steve Barnett <steve@naga.co.za>
2014-01-15 14:57:45 +0200

    Removed reference to meetup

--- the-lab.php
+++ the-lab.php
@@ -19,9 +19,6 @@ require('_header.php');
   <p>The lab has a permanent home at my workplace: <a href="http://www.unboxedconsulting.com/">Unboxed Consulting</a> in Constantia. <a href="http://goo.gl/maps/SxLkY">View Google Map of the location</a>.<br />
   If you'd like to come and do some testing, please <strong>make an appointment</strong> by dropping me a line to <a class="email" href="mailto:nomad@devicelab.co.za">nomad@devicelab.co.za</a>.</p>
 
-  <p>The listing of sessions from 2012 is up at <a href="http://www.meetup.com/Nomad-Device-Lab/">Meetup.com/Nomad-Device-Lab</a>.</p>
-
-
 </section>
 
 <section>
